689d376c436b024b0f56e6c44a8905b55c492a9d
Nick <slipperygate@gmail.com>
2021-05-23 22:41:02 +0300

    more consolidation for requirements. added changing marker colors, about modal and collapsible sidebar. added bootstrap css to static files.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,13 +4,36 @@ import DeviceMessagesService from './services/device-messages.service.js';
 
 const app = Vue.createApp({
     mounted() {
+        let self = this;
+
         // setup map
-        let myMap = L.map('map-id').setView([51.505, -0.09], 13);
-        myMap.setView([40.3600019617025, -111.89497947692871], 12);
+        let mainMap = L.map('map-id').setView([51.505, -0.09], 13);
+        mainMap.setView([40.3600019617025, -111.89497947692871], 12);
 
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18
-        }).addTo(myMap);
+        }).addTo(mainMap);
+
+        mainMap.on('click', e => {
+            this.navToggled = true;
+            self.markers.forEach((m) => {
+                m.setIcon(blueMarker);
+            })
+        });
+
+        // define markers
+        const [blueMarker, purpleMarker] = ['marker-icon.png', 'marker2-icon.png'].map((filename) => {
+            return L.icon({
+                iconUrl: `vendor/leaflet/images/${filename}`,
+                shadowUrl: 'vendor/leaflet/images/marker-shadow.png',
+
+                iconSize: [24, 42], // size of the icon
+                shadowSize: [41, 41], // size of the shadow
+                iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+                shadowAnchor: [4, 41],  // the same for the shadow
+                // popupAnchor:  [3, 20] // point from which the popup should open relative to the iconAnchor
+            });
+        });
 
         // get devices data
         // DeviceService.getMockDevices().then(
@@ -34,10 +57,10 @@ const app = Vue.createApp({
                     });
                 });
 
-                // Add device marker to map
-                this.markers = devices.map((device) => {
+                // Add device markers to map
+                self.markers = devices.map((device) => {
                     // Create
-                    let marker = new L.Marker(myMap.getCenter())
+                    let marker = new L.Marker(mainMap.getCenter())
                     // Label
                     marker.bindTooltip(device.name, {
                         permanent: true,
@@ -46,8 +69,14 @@ const app = Vue.createApp({
                     // Set marker location - takes [latitude, longitude]
                     let ls = device.getLatestCoordinates();
                     marker.setLatLng([ls.lat, ls.lon]);
-                    marker.addTo(myMap);
-                    marker.on('click', e => vm.showDeviceStatus(device));
+                    marker.addTo(mainMap);
+                    marker.on('click', e => {
+                        vm.showDeviceStatus(device);
+                        self.markers.forEach((m) => {
+                            m.setIcon(blueMarker);
+                        })
+                        marker.setIcon(purpleMarker);
+                    });
                     // Add
                     return marker;
                 });
@@ -63,15 +92,27 @@ const app = Vue.createApp({
     },
     data() {
         return {
+            navToggled: true,
+            showModal: false,
+            title: "None",
             position: null,
             status: null
         }
     },
     methods: {
+        toggleNavbar(event) {
+            this.navToggled = !this.navToggled;
+        },
+        setModalOpen(show, event) {
+            console.debug("event: ", event);
+            event && event.preventDefault();
+            this.showModal = show;
+        },
         showDeviceStatus(device) {
             this.title = device.name;
             this.position = device.getLatestCoordinates();
             this.status = device.getLatestStatus();
+            this.navToggled = false;
         }
     }
 });
--- src/main/resources/static/services/devices.service.js
+++ src/main/resources/static/services/devices.service.js
@@ -12,19 +12,4 @@ export default class DeviceService {
             }
         )
     }
-
-    // static getMockDevices() {
-    //     return new Promise((resolve) => {
-
-    //         let d1 = new Device(1, "Ludwig");
-    //         d1.setGlobalPosition(Date.now(), 40.3601491164367, -111.89167499542236, 0, 70);
-    //         d1.setDeviceStatus(Date.now(), 60 * 60 * 1000, 78);
-
-    //         let d2 = new Device(2, "Friedrich");
-    //         d2.setGlobalPosition(Date.now(), 40.3701491164367, -111.92167499542236, 0, 70);
-    //         d2.setDeviceStatus(Date.now(), 112 * 60 * 1000, 62);
-
-    //         return resolve([d1, d2]);
-    //     });
-    // }
 }
